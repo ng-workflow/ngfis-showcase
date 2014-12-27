8cbf00c76824731194038157f4f0ee7817fa1bff
TZ <atian25@qq.com>
2014-12-27 13:59:58 +0800

    简化代码
    Signed-off-by: TZ <atian25@qq.com>

--- components/angular-lazyload/angular-lazyload.js
+++ components/angular-lazyload/angular-lazyload.js
@@ -14,28 +14,6 @@
     throw new Error('Missing angular');
   }
 
-  /**
-   * register `angular-lazyload` module & `$lazyload` service
-   */
-  angular.module('angular-lazyload', [], ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
-    function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
-      //register `$lazyload` service
-      $provide.factory('$lazyload', ['$rootScope', '$q', function($rootScope, $q){
-        //hold provide's refs, because after ng bootstrap, you can't use `app.controller` to dynamic register controller.
-        var hooks = {
-          controller: $controllerProvider.register,
-          directive: $compileProvider.directive,
-          filter: $filterProvider.register,
-          factory: $provide.factory,
-          service: $provide.service,
-          decorator: $provide.decorator
-        };
-        return hooks;
-        //return new LazyLoadProvider($rootScope, $q, register);
-      }]);
-    }
-  ]);
-
   var origMethod = angular.bind(angular, angular.module);
   angular.module = function(name, requires, configFn){
     if(!requires){
@@ -43,26 +21,45 @@
       return origMethod(name);
     }else{
       //声明模块
-      requires.unshift('angular-lazyload');
       var m = origMethod(name, requires, configFn);
       initHooks(m);
       initTemplate(m);
-      initRoute(m, requires);
+      if(requires.indexOf('ngRoute') !== -1) {
+        initRoute(m);
+      }
       return m;
     }
   };
 
+  /**
+   * override hooks(controller/service/...) to support dynamic register
+   */
   function initHooks(app){
-    app.run(['$lazyload', function($lazyload){
-      var hooks = $lazyload;
+    var hooks = {};
+    app.config(['$controllerProvider', '$compileProvider', '$filterProvider', '$provide', function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
+      //hold provide's refs, because after ng bootstrap, you can't use `app.controller` to dynamic register controller.
+      hooks = {
+        controller: $controllerProvider.register,
+        directive: $compileProvider.directive,
+        filter: $filterProvider.register,
+        factory: $provide.factory,
+        service: $provide.service,
+        decorator: $provide.decorator
+      };
+    }]);
+
+    app.run(function(){
       for (var key in hooks) {
         if (hooks.hasOwnProperty(key)) {
           app[key] = hooks[key];
         }
       }
-    }]);
+    });
   }
 
+  /**
+   * add `app.template(id, content)` interface
+   */
   function initTemplate(app){
     var cacheHolder;
     app.run(["$templateCache", function($templateCache) {
@@ -82,100 +79,67 @@
     };
   }
 
-  function initRoute(app, requires){
-    if(requires.indexOf('ngRoute') !== -1){
-      //app.config(['$routeProvider', function($routeProvider){
-      //  var origMethod = angular.bind($routeProvider, $routeProvider.when);
-      //  $routeProvider.when = function(path, route){
-      //    route = route || {};
-      //    if(!route.controller && path){
-      //      route.controller = path.toLowerCase().replace(/\/(.)/g, function(m, g){return g.toUpperCase();}) + 'Ctrl';
-      //    }
-      //    if(typeof route.controller === 'string') {
-      //      var moduleId = route.id || route.moduleId || ('modules/' + route.controller.replace(/Ctrl$/, '').replace(/([A-Z])/g, function(m){return "/" + m.toLowerCase();}));
-      //      route.resolve = route.resolve || {};
-      //
-      //      var templateDefer;
-      //
-      //      route.resolve.$module = ['$q', '$timeout', '$route', function ($q, $timeout, $route) {
-      //        var defer = $q.defer();
-      //        templateDefer = $q.defer();
-      //        console.log('require:', moduleId)
-      //        require.async(moduleId, function(m){
-      //          console.log(m)
-      //          $timeout(function(){
-      //            if(m.template){
-      //              //route.locals.$template = m.template;
-      //              templateDefer.resolve(m.template);
-      //            }
-      //            defer.resolve(m);
-      //          });
-      //        });
-      //        return defer.promise;
-      //      }];
-      //      if(!route.template && !route.templateUrl){
-      //        route.resolve.$template = function(){
-      //          return templateDefer.promise;
-      //        };
-      //      }
-      //    }
-      //    return origMethod.call($routeProvider, path, route);
-      //  }
-      //}]);
+  /**
+   * support dynamic loading for ngRoute
+   */
+  function initRoute(app){
+    app.run(['$rootScope', '$q', '$timeout', function($rootScope, $q, $timeout){
+      //listen to route change event to hook
+      $rootScope.$on('$routeChangeStart', function(e, target) {
+        //console.debug(e, '|', target);
+        var route = target && target.$$route;
+        if(route){
+          //controller not define, /menu/list -> menuListCtrl
+          if(!route.controller && route.originalPath) {
+            route.controller = camelCase(route.originalPath) + 'Ctrl';
+          }
+          //controller is string
+          if(typeof route.controller === 'string') {
+            var moduleId = route.id || route.moduleId || ('modules/' + pathCase(route.controller.replace(/Ctrl$/, '').replace(/^\//, '')));
+            route.resolve = route.resolve || {};
 
-      app.run(['$rootScope', '$q', '$timeout', function($rootScope, $q, $timeout){
-        $rootScope.$on('$routeChangeSuccess', function(e, target){
-          console.debug('$routeChangeSuccess', e, target);
-        });
-        //listen to route change event to hook
-        $rootScope.$on('$routeChangeStart', function(e, target) {
-          //console.debug(e, '|', target);
-          var route = target && target.$$route;
-          if(route){
-            //controller not define, /menu/list -> menuListCtrl
-            if(!route.controller && route.originalPath) {
-              route.controller = camelCase(route.originalPath) + 'Ctrl';
-            }
-            //controller is string
-            if(typeof route.controller === 'string') {
-              var moduleId = route.id || route.moduleId || ('modules/' + pathCase(route.controller.replace(/Ctrl$/, '').replace(/^\//, '')));
-              route.resolve = route.resolve || {};
+            var moduleDefer = $q.defer();
+            var templateDefer = $q.defer();
 
-              var moduleDefer = $q.defer();
-              var templateDefer = $q.defer();
+            //load module promise
+            route.resolve.$module = ['$timeout', function ($timeout) {
+              require.async(moduleId, function (m){
+                //$timeout(function(){
+                  templateDefer.resolve(m && m.template);
+                  moduleDefer.resolve(m);
+                //});
+              });
+              return moduleDefer.promise;
+            }];
 
-              //load module promise
-              route.resolve.$module = ['$timeout', function ($timeout) {
-                require.async(moduleId, function (m){
-                  //$timeout(function(){
-                    templateDefer.resolve(m && m.template);
-                    moduleDefer.resolve(m);
-                  //});
-                });
-                return moduleDefer.promise;
-              }];
-
-              //template promise
-              if(!route.template && !route.templateUrl){
-                route.resolve.$template = function(){
-                  return templateDefer.promise;
-                };
-              }
+            //template promise
+            if(!route.template && !route.templateUrl){
+              route.resolve.$template = function(){
+                return templateDefer.promise;
+              };
             }
           }
-        });
-      }]);
-    }else{
+        }
+      });
 
-    }
+      $rootScope.$on('$routeChangeError', function(nextRoute, lastRoute, error){
+        console.error('$routeChangeError', nextRoute, lastRoute, error);
+      });
+    }]);
   }
 
+  /**
+   * change `/menu/list` -> 'menuList'
+   */
   function camelCase(path){
     return path.toLowerCase().replace(/^\//, '').replace(/\/(.)/g, function (m, g) {
       return g.toUpperCase();
     });
   }
 
+  /**
+   * change 'menuList' -> `/menu/list`
+   */
   function pathCase(str){
     return str.replace(/([A-Z])/g, function(m){
       return "/" + m.toLowerCase();
